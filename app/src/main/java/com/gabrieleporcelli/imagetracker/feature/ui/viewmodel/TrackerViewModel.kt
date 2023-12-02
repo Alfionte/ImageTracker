977a13ae770230444dbaf6f21922e8cd35d6333a
alfionte <gabriele.porcelli@gmail.com>
2023-12-02 23:47:15 +0000

    Add tracked images and actions to the TrackerViewModel

--- app/src/main/java/com/gabrieleporcelli/imagetracker/feature/ui/viewmodel/TrackerViewModel.kt
+++ app/src/main/java/com/gabrieleporcelli/imagetracker/feature/ui/viewmodel/TrackerViewModel.kt
@@ -1,11 +1,13 @@
 package com.gabrieleporcelli.imagetracker.feature.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.gabrieleporcelli.imagetracker.core.domain.model.TrackedImage
 import com.gabrieleporcelli.imagetracker.feature.domain.TrackerState
 import com.gabrieleporcelli.imagetracker.feature.domain.TrackerViewAction
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,12 +18,16 @@ class TrackerViewModel @Inject constructor(
     private val _state: MutableStateFlow<TrackerState> = MutableStateFlow(TrackerState.NoPermission)
     val state = _state.asStateFlow()
 
+    private val _trackedImages: MutableStateFlow<List<TrackedImage>> = MutableStateFlow(emptyList())
+    val trackedImages = _trackedImages.asStateFlow()
 
     fun onAction(action: TrackerViewAction) {
         when (action) {
-            TrackerViewAction.Start -> TODO()
-            TrackerViewAction.Stop -> TODO()
-            TrackerViewAction.Clean -> TODO()
+            TrackerViewAction.OnStart -> _state.update { TrackerState.TrackerStarted }
+            TrackerViewAction.OnStop -> _state.update { TrackerState.TrackerStopped }
+            TrackerViewAction.OnClean -> TODO()
+            TrackerViewAction.OnPermissionClicked -> _state.update { TrackerState.PermissionPermanentlyDenied }
+            TrackerViewAction.OnPermissionSettingsClicked -> _state.update { TrackerState.TrackerStopped }
         }
     }
 }
