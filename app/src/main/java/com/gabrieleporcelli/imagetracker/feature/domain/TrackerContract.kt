37401f05fd86929fe282fc10eb14059846851046
alfionte <gabriele.porcelli@gmail.com>
2023-12-02 22:14:02 +0000

    Update tracker state

--- app/src/main/java/com/gabrieleporcelli/imagetracker/feature/domain/TrackerContract.kt
+++ app/src/main/java/com/gabrieleporcelli/imagetracker/feature/domain/TrackerContract.kt
@@ -4,13 +4,13 @@ package com.gabrieleporcelli.imagetracker.feature.domain
 sealed interface TrackerState {
     data object NoPermission : TrackerState
     data object PermissionPermanentlyDenied : TrackerState
-
-    data object TrackerStarted : TrackerState
     data object TrackerStopped : TrackerState
+    data object TrackerStarted : TrackerState
 }
 
 sealed interface TrackerViewAction {
     data object Start : TrackerViewAction
     data object Stop : TrackerViewAction
+    data object Clean : TrackerViewAction
 }
 
